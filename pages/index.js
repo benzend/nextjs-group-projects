a6acb7c116656954b6af4049b2cc931f4771d9d1
Benjamin <1234hteimaj4321@gmail.com>
2021-01-08 01:45:20 -0700

    Added boilerplate
    Added:
    - Navigation to pages
    - BoilerPlate for each page
    - TypeScript
    - Redux & Types
    - Material UI + Icons
    - Jest

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  link: {
+    fontSize: "5rem",
+    cursor: "pointer",
+    textShadow: "0 10px 5px #0005",
+    transition: "all .3s ease",
+    ["&:hover"]: {
+      textShadow: "0 50px 10px #0002",
+      transform: "scale(1.1)",
+    },
+  },
+});
 
 export default function Home() {
+  const classes = useStyles();
   return (
     <div className={styles.container}>
       <Head>
@@ -9,57 +25,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Link href="/maserati">
+        <div className={classes.link}>Maserati</div>
+      </Link>
+      <Link href="/social-app">
+        <div className={classes.link}>Social App</div>
+      </Link>
     </div>
-  )
+  );
 }
---
+++ pages/social-app/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+
+export default function Home() {
+  return (
+    <>
+      <Typography variant="h1" color="textPrimary">
+        Social App Home
+      </Typography>
+    </>
+  );
+}
